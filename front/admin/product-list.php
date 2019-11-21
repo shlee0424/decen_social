54b1d165f981330d6afe154d4f9f75b253b11fd0
shlee0424 <scv7200@hanmail.net>
2019-11-21 18:20:11 +0900

    fix product-list, create product-add, product-preview

--- front/admin/product-list.php
+++ front/admin/product-list.php
@@ -20,7 +20,7 @@
     if(!empty($_REQUEST["srchTxt"])){
       if(strlen($_REQUEST["srchTxt"]) > 0){
         $srchTxt =$_REQUEST["srchTxt"];
-        $whereStatement = " where (title like'%$srchTxt%')";
+        $whereStatement = " where (title like '%$srchTxt%')";
         
       }
     }
@@ -39,7 +39,6 @@
     $endpg = ceil(intval($cntrow["total"])/$pageDataAmt);
 
   $productSQL = "select * from db_product $whereStatement order by regYHS DESC limit $startblock, $pageDataAmt";
-  echo("productSQL : $productSQL");
   $result = mysqli_query($kiki_conn, $productSQL);
 ?>
 <html lang="ko">
@@ -55,57 +54,7 @@
 </head>
 
 <body class="bg-dark text-light">
-  <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary shadow-sm" style="background: linear-gradient(135deg, #222 0%, #343a40 100%);">
-    <a class="navbar-brand mr-auto mr-lg-3" href="#"><img src="../assets/img/logo-light.svg" style="height: 2rem;"></a>
-    <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">회원 리스트</a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="#">상품 리스트</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">상품 클릭 현황</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">인스턴스 정보</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">상품 리스트</a>
-        </li>
-      </ul>
-      <form class="form-inline my-2 my-lg-0 ml-lg-auto">
-        <div class="input-group">
-          <input type="text" id="srchTxt" class="form-control" placeholder="검색">
-          <div class="input-group-append">
-            <button onclick="doSearch()" class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fas fa-search"></i></button>
-          </div>
-        </div>
-      </form>
-    </div>
-  </nav>
-
-  <div class="nav-scroller bg-secondary navbar-dark shadow">
-    <nav class="nav nav-underline">
-      <a class="nav-link active text-light" href="#">Dashboard</a>
-      <a class="nav-link text-light" href="#">
-        Friends
-        <span class="badge badge-pill bg-light align-text-bottom text-secondary">27</span>
-      </a>
-      <a class="nav-link text-light" href="#">Explore</a>
-      <a class="nav-link text-light" href="#">Suggestions</a>
-      <a class="nav-link text-light" href="#">Link</a>
-      <a class="nav-link text-light" href="#">Link</a>
-      <a class="nav-link text-light" href="#">Link</a>
-      <a class="nav-link text-light" href="#">Link</a>
-      <a class="nav-link text-light" href="#">Link</a>
-    </nav>
-  </div>
+  <? include "./inc/header.php";?>
 
   <main role="main" class="container">
 
@@ -146,7 +95,7 @@
           </ul>
         </nav>
          
-        <p class="text-right"><a href="#" class="btn btn-secondary">상품 등록</a></p>
+        <p class="text-right"><a href="product-add.php" class="btn btn-secondary">상품 등록</a></p>
       </div>
     </div>
   </main>
